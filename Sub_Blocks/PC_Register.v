8c5c144ba601f57f2daa6d1dc0feeb3646f70c35
Sai Srikar <saisrikar109@gmail.com>
2025-07-20 00:25:37 +0530

    Update PC_Register.v

--- Sub_Blocks/PC_Register.v
+++ Sub_Blocks/PC_Register.v
@@ -1,20 +1,16 @@
 `timescale 1ns / 1ps
 
 
-module PC_Register(
-    input clk,
-    input reset,
-    input [`WORD_SIZE-1:0] pc_in,
-    output reg [`WORD_SIZE-1:0] pc_out
-);
-    initial begin
-        pc_out = 32'h00000000;
-    end
-
+module PC_Register( input clk, reset, 
+                    input [`WORD_SIZE-1:0] pc_in, 
+                    output reg [`WORD_SIZE-1:0] pc_out
+                  );
+                  
     always @(posedge clk or posedge reset) begin
         if (reset)
             pc_out <= 32'h00000000;
         else
             pc_out <= pc_in;
     end
+    
 endmodule
